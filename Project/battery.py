976a4b566ed5febc73ced622855f341f516ea0a9
ubuntu <child1.andy@gmail.com>
2024-11-24 12:37:53 +0200

    Updated battery.py

--- Project/battery.py
+++ Project/battery.py
@@ -4,4 +4,5 @@ tello = Tello()
 tello.connect()
 
 print("Battery : " + str(tello.get_battery()))
-print("Battery : " + str(tello.get_current_state()))
+print("")
+print("Status : " + str(tello.get_current_state()))
